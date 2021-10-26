816861cac98d28456de14116bd937b8b4f67cf4c
Harsh Banthiya <hbanthiya@gmail.com>
2021-10-26 09:20:54 -0400

    added some missing stuff from the merging

--- Tree_pseudo/cmd/cmd.c
+++ Tree_pseudo/cmd/cmd.c
@@ -27,13 +27,11 @@ void    command_store_argv(t_cmd *command, t_node *arg_node, t_list *env_list)
         if (i == 0 && ft_strchr(arg_node->data, '/'))
             command->has_path = 1;
         
-        /* I NEED TO MAKE THESE THREE FUNCTIONS < THE FUCKING BANE OF MY EXISTENCE, THE LAST LINK IN THE WHOLE EXECUTION IDEA 
+        // I NEED TO MAKE THESE THREE FUNCTIONS < THE FUCKING BANE OF MY EXISTENCE, THE LAST LINK IN THE WHOLE EXECUTION IDEA 
         arg_node->data = replace_env(arg_node->data, env_list);
-        arg_node->data = strip_quotes(arg_node->data); (vrai)
+        arg_node->data = strip_quotes(arg_node->data);
         arg_node->data = replace_home_path(arg_node->data, env_list);
-        */
         command->argv[i] = ft_strdup(arg_node->data);
-
         arg_node = arg_node->right_child;
         i++;
     }
