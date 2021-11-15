5855e3e6449d81b398c9cffaa22e37ce4df61778
Harsh Banthiya <hbanthiya@gmail.com>
2021-11-15 10:06:56 -0500

    typo

--- srcs/cmd/convert_ast_cmd.c
+++ srcs/cmd/convert_ast_cmd.c
@@ -5,134 +5,149 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbanthiy <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/12 12:06:01 by hbanthiy          #+#    #+#             */
-/*   Updated: 2021/11/15 09:35:36 by hbanthiy         ###   ########.fr       */
+/*   Created: 2021/11/10 10:53:03 by hbanthiy          #+#    #+#             */
+/*   Updated: 2021/11/15 10:06:15 by hbanthiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <string.h>
-#include "../../libft/libft.h"
-#include "../../includes/ms_execution.h"
-#include "../../includes/ms_environment.h"
 #include "../../includes/minishell.h"
 
-int	put_redir_errmsg_and_ret(int ret_value, int fd, char *msg)
-{
-	char	*fd_str;
+/* 
+    set values of command->execandargs base on string_node
+    string ::= expandable <no_space> string | expandable | not_expandable <no_space> string
+                | not_expandable | expandable_quoted <no_space> string | expandable_quoted
+*/
 
-	fd_str = ft_itoa(fd);
-	if (!fd_str)
-		put_minish_err_msg_and_ret(ERROR, "redirection", "ft_strjoin() failed");
-	put_minish_err_msg(fd_str, msg);
-	free(fd_str);
-	return (ret_value);
+int     cmd_process_string_node(
+    t_parse_node_string *string_node, t_command *command)
+{
+    char    **splitted_env_val;
+    char    **strarr;
+
+    if (command->exec_and_args && command->exec_and_args[0]
+        && !ft_strcmp(command->exec_and_args[0], "export"))
+        splitted_env_val = expand_string_node(string_node, true);
+    else 
+        splitted_env_val = expand_string_node(string_node, false);
+    strarr = ptrarr_merge((void **)command->exec_and_args, (void **)splitted_env_val);
+    free(splitted_env_val);
+    if (!strarr)
+        return (-1);
+    free((void **)command->exec_and_args);
+    command->exec_and_args = strarr;
+    return (0);
+}
+
+/* 
+    set values of command->input_file_path or output_file_path
+    based on the redirection node
+    Mimicking bash behaviour, environment variables expansions will not be done here
+    but right before execution.
+    redirection := "<" string | ">" string | ">>" string
+*/
+
+int cmd_process_redirection_node(t_parse_node_redirection *redirection_node,
+    t_command *command)
+{
+    int     redirection_type;
+    char    *text;
+    int     fd;
+    int     status;
+
+    redirection_type = redirection_node->type;
+    text = string_node2string(redirection_node->string_node->content.string,
+            redirection_type != TOKTYPE_HEREDOCUMENT);
+    fd = redirection_node->fd;
+    status = 0;
+    if (!text)
+        return (-1);
+    if (redirection_type == TOKTYPE_INPUT_REDIRECTION)
+        status = cmd_add_inredirect(command, text, fd);
+    else if (redirection_type = TOKTYPE_OUTPUT_REDIRECTION)
+        status = cmd_add_outredirect(command, text, fd, false);
+    else if (redirection_type == TOKTYPE_OUTPUT_APPENDING)
+        status = cmd_add_outredirect(command, text, fd, true);
+    else if (redirection_type == TOKTYPE_HEREDOCUMENT)
+        status = cmd_add_heredoc(command, text, fd, 
+                    cmd_is_heredoc_expandable(redirection_node));
+    free((void *)text);
+    return (status);
+}
+
+/* 
+    Parse Argument Node and set values of command
+    arguments ::= redirection | redirection arguments | string
+                  | string arguments
+*/
+
+int     cmd_process_args_node(
+    t_parse_node_arguments *args_node, t_command *command)
+{
+    if (args_node->string_node)
+        return (cmd_process_string_node(
+            args_node->string_node->content.string, command));
+    else if (args_node->redirection_node)
+        return (cmd_process_redirection_node(
+            args_node->redirection_node->content.redirection, command));
+    else 
+        return (1);
+    return (0);
 }
 
 /*
- * Expand variable of the string passed for redirection.
- * If error is occurred, put error message to stderr.
- *
- * red_target: The string of redirection node.
- *
- * Return: Filepath if variable expansion is succeeded.
- *   If the result of the environment variable expansion
- *     can be decomposed into two strings, an NULL is returned.
- */
-char	*expand_redirect_filepath(char *red_target)
+    Convert AST commands node to command object
+    command ::= arguments
+*/
+
+t_command   *ast_cmd2cmd(t_parse_node_command *cmd_node)
 {
-	char	*expanded_str;
-	char	**splitted_expanded_str;
-	char	*filepath;
+    t_command               *command;
+    t_parse_node_arguments  *args_node;
 
-	expanded_str = expand_env_var(red_target);
-	splitted_expanded_str = split_expanded_str(expanded_str);
-	free(expanded_str);
-	if (!splitted_expanded_str
-		|| ptrarr_len((void **)splitted_expanded_str) != 1)
-	{
-		put_minish_err_msg(red_target, "ambiguous redirect");
-		free_ptrarr((void **)splitted_expanded_str);
-		return (NULL);
-	}
-	filepath = ft_strdup(splitted_expanded_str[0]);
-	free_ptrarr((void **)splitted_expanded_str);
-	return (filepath);
-}
-
-int	open_file_for_redirect(t_cmd_redirection *red,
-	int open_flags, mode_t open_mode)
-{
-	char	*filepath;
-	int		fd;
-
-	filepath = expand_redirect_filepath((char *)red->filepath);
-	if (!filepath)
-		return (ERROR);
-	fd = open(filepath, open_flags, open_mode);
-	if (fd == -1)
-		put_minish_err_msg(filepath, strerror(errno));
-	free(filepath);
-	return (fd);
+    command = cmd_init_cmd(NULL);
+    check_malloc_success("ast2cmd", command);
+    args_node = cmd_node->arguments_node->content.arguments;
+    while (args_node)
+    {
+        if (cmd_process_args_node(args_node, command))
+        {
+            cmd_free_cmd(command);
+            return (NULL);
+        }
+        if (args_node->rest_node)
+            args_node = args_node->rest_node->content.arguments;
+        else 
+            args_node = NULL;
+    }
+    return (command);
 }
 
 /*
- * open input file as stdin if command->input_file_path is exist
- *
- * command: command.
- *
- * return: return -1 if error has occurred, otherwise, return 0.
- */
-int	cmd_set_input_file(t_fd_reds_list *fd_red_list)
+    Convert Ast command node to command object
+
+    pipcmds: piped_commands
+    piped_commands ::= command "|" piped_commands | command
+*/
+
+t_command   *pipcmd2cmd(t_parse_node_pipcmds *pipcmds)
 {
-	int					fd;
-	t_cmd_redirection	*red;
+    t_command       *commands;
+    t_command       *command;
 
-	while (fd_red_list)
-	{
-		close(fd_red_list->heredoc_pipe[1]);
-		red = fd_red_list->reds;
-		while (red)
-		{
-			if (!red->is_heredoc)
-			{
-				fd = open_file_for_redirect(red, O_RDONLY, 0);
-				if (fd == ERROR || dup2(fd, red->fd) == -1)
-					return (ERROR);
-			}
-			else if (!red->next && fd_red_list->heredoc_pipe[0] != -1
-				&& dup2(fd_red_list->heredoc_pipe[0], fd_red_list->fd) == -1)
-				return (ERROR);
-			red = red->next;
-		}
-		close(fd_red_list->heredoc_pipe[0]);
-		fd_red_list = fd_red_list->next;
-	}
-	return (0);
-}
-
-/*
- * open output file as stdout if command->output_file_path is exist
- *
- * command: command.
- *
- * return: return -1 if error has occurred, otherwise, return 0.
- */
-int	cmd_set_output_file(t_command *command)
-{
-	int					fd;
-	int					flag_open;
-	t_cmd_redirection	*red;
-
-	red = command->output_redirections;
-	while (red)
-	{
-		/* Needs work with file mode and permissions and what to open */
-	}
-	return (0);
+    commands = NULL;
+    while (pipcmds)
+    {
+        command = ast_cmd2cmd(pipcmds->command_node->content.command);
+        if (!command)
+        {
+            cmd_free_cmd(commands);
+            return (NULL);
+        }
+        cmd_add_pipcmd(&commands, command);
+        if (pipcmds->next)
+            pipcmds = pipcmds->next->content.piped_commands;
+        else 
+            pipcmds = NULL;
+    }
+    return (commands);
 }
