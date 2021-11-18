74939043d94086bb69b14b9b6886683e0f171608
Harsh Banthiya <hbanthiya@gmail.com>
2021-11-18 12:55:44 -0500

    tester added

--- srcs/builtin/unset.c
+++ srcs/builtin/unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sfournie <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*   By: hbanthiy <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 19:27:51 by sfournie          #+#    #+#             */
-/*   Updated: 2021/11/17 17:50:18 by sfournie         ###   ########.fr       */
+/*   Updated: 2021/11/18 10:48:56 by hbanthiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,27 +60,49 @@ int	ft_unset_var(char *key, t_dlist **lst)
 	}
 	return (0);
 }
+static void put_unset_errmsg(char *cmd_name, char *keyname)
+{
+	char 	*tmp;
+	char 	*errmsg;
+	
+	tmp = ft_strjoin("`", keyname);
+	if (!tmp)
+		put_minish_err_msg_and_exit(1, cmd_name, "ft_strjoin() failed");
+	errmsg = ft_strjoin(tmp, "': not a valid identifier");
+	free(tmp);
+	if (!errmsg)
+		put_minish_err_msg_and_exit(1, cmd_name, "ft_strjoin() failed");
+	put_minish_err_msg(cmd_name, errmsg);
+	free(errmsg);
+}
 
 int	ft_unset(char **argv)
 {
 	int		i;
 	int		exit_code;
 	t_dlist	**env;
+	bool	is_key_ok;
 
 	exit_code = 0;
 	env = get_env();
+	is_key_ok = true;
 	if (env == NULL)
 		return (1);
-	if (argv[1] == NULL)
-		return(error_builtin("unset", NULL, "not enough arguments", 1));
+	//if (argv[1] == NULL)
+		//return(error_builtin("unset", NULL, "not enough arguments", 1));
 	i = 1;
 	while (argv[i])
 	{
 		if (var_is_valid_key(argv[i]))
-			ft_unset_var(argv[i++], env);
+			ft_unset_var(argv[i], env);
 		else
-			exit_code = error_builtin("unset", argv[i++], "not a valid identifier", 1);
-		
+		{
+			is_key_ok = false;
+			put_unset_errmsg(argv[0], argv[i]);
+		}
+		i++;		
 	}
-	return (exit_code);
+	if (!is_key_ok)
+		return (1);
+	return (0);
 }
