498f0a39af16a7b0c3a392aa2945f172b616c7f3
sfournie <sfournie@student.42quebec.com>
2021-12-02 14:35:22 -0500

    tentative pre-fork check

--- srcs/cmd/cmd_exec_cmds.c
+++ srcs/cmd/cmd_exec_cmds.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cmd_exec_cmds.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hbanthiy <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*   By: sfournie <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 09:06:29 by hbanthiy          #+#    #+#             */
-/*   Updated: 2021/12/02 12:04:15 by hbanthiy         ###   ########.fr       */
+/*   Updated: 2021/12/02 14:34:24 by sfournie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,41 @@
 #include "../../includes/ms_environment.h"
 #include "../../includes/ms_execution.h"
 #include "../../includes/ms_builtin.h"
+
+static int    is_valid_command(char *name)
+{
+    char    **dirs;
+    char    *value;
+    char    *full_path;
+    int        i;
+
+    value = get_var_value("PATH", *get_env());
+	if (is_builtin(name))
+		return (1);
+    if (value)
+        dirs = ft_split(value, ':');
+    else
+        dirs = NULL;
+    if (!access("name", X_OK))
+        return (1);
+    i = 0;
+    while (dirs[i])
+    {
+        full_path = (char *)ft_calloc(ft_strlen(dirs[i]) + ft_strlen(name) + 2, sizeof(char));
+        full_path = ft_strcat(full_path, dirs[i]); 
+        full_path = ft_strcat(full_path, "/");
+        full_path = ft_strcat(full_path, name); 
+        if (!access(full_path, X_OK))
+        {
+            ft_free(full_path);
+            return (1);
+        }
+        ft_free(full_path);
+        i++;
+    }
+    printf("invalid command");
+    return (0);
+}
 
 static int	cmd_connect_pipe(
 	int pipe_prev_fd[2], int pipe_fd[2])
@@ -81,6 +116,8 @@ static int	cmd_exec_one_command(t_command *current_cmd,
 		return (put_err_msg_and_ret("error pipe()"));
 	if (current_cmd->exec_and_args)
 		ft_export_var("_", (char *)current_cmd->exec_and_args[0], get_env());
+	if (!is_valid_command((char *)current_cmd->exec_and_args[0]))
+		return (0);
 	pid = fork();
 	if (pid < 0)
 		return (put_err_msg_and_ret("error fork()"));
