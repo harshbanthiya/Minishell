97044e875dc54ba41c97d1f945f852df41b59f50
sfournie <sfournie@student.42quebec.com>
2021-12-03 13:13:27 -0500

    added the prompt fix

--- srcs/shell/terminal.c
+++ srcs/shell/terminal.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   terminal.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hbanthiy <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*   By: sfournie <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 12:32:05 by sfournie          #+#    #+#             */
-/*   Updated: 2021/12/03 09:43:44 by hbanthiy         ###   ########.fr       */
+/*   Updated: 2021/12/03 13:11:33 by sfournie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,27 @@ char	*get_prompt(void)
 {
 	t_shell	*sh;
 	char	*prompt;
+	char	*pwd;
 
 	sh = get_shell();
 	if (!sh)
 		return (NULL);
 	prompt = sh->prompt;
 	ft_strcpy(prompt, PROMPT);
-	ft_strlcat(prompt, C_CYAN, 150);
-	ft_strlcat(prompt, ft_strrchr(get_pwd(), '/'), 150);
+	ft_strlcat(prompt, C_CYAN, PROMPT_SIZE);
+	pwd = ft_strrchr(get_pwd(), '/');
+	if (ft_strlen(pwd) > 15)
+	{
+		pwd[20] = '\0';
+		ft_strcat(prompt, pwd);
+		ft_strcat(prompt, "..");
+	}
+	else
+		ft_strlcat(prompt, ft_strrchr(get_pwd(), '/'), PROMPT_SIZE);
 	append_git(prompt);
-	ft_strlcat(prompt, C_CYAN, 150);
-	ft_strlcat(prompt, " > ", 150);
-	ft_strlcat(prompt, C_NO, 150);
+	ft_strlcat(prompt, C_CYAN, PROMPT_SIZE);
+	ft_strlcat(prompt, " > ", PROMPT_SIZE);
+	ft_strlcat(prompt, C_NO, PROMPT_SIZE);
 	return (prompt);
 }
 
